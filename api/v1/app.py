de52aecc2e359d5327dbaa14176f8dbdc73b894b
Felipe Prado <jfpc11@misena.edu.co>
2021-09-17 11:41:59 -0500

    adding the 404 response as json

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """creating an api"""
 
+from flask.helpers import make_response
+from flask.json import jsonify
 from api.v1.views import app_views
 from flask import Flask
 from os import getenv
@@ -8,6 +10,11 @@ from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 @app.teardown_appcontext
