5083ca7ce683e471aa826baeaf136f8e7122c526
SebastianMH14 <sebasmh2002@hotmail.com>
2021-09-17 10:31:20 -0500

    add new route counting the objects diferents type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,28 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+classes = {"amenities": Amenity, "cities": City,
+           "places": Place, "reviews": Review, "states": State, "users": User}
 
 
 @app_views.route("/status")
 def status_api():
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def count_obj():
+    obj_stats = {}
+    for key, value in classes.items():
+        count = storage.count(value)
+        obj_stats[key] = count
+    return jsonify(obj_stats)
