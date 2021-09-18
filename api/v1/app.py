8ec13796b529ca348900e3d94f3ba01881f6b07e
SebastianMH14 <sebasmh2002@hotmail.com>
2021-09-17 21:41:56 -0500

    trying to fix cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app_views)
+CORS(app_views, resources={r"/api/*": {"origins": "*"}})
 
 
 @app.errorhandler(404)
